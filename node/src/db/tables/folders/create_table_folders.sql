8b8f93973a98f4bd54c6eb43dbd613c1bf5786ee
David Xavier <davidxavier7512@gmail.com>
2025-10-09 23:47:17 +0100

    Q

--- node/src/db/tables/folders/create_table_folders.sql
+++ node/src/db/tables/folders/create_table_folders.sql
@@ -2,7 +2,6 @@ CREATE TABLE folders (
   id                SERIAL        PRIMARY KEY,
   description       TEXT          NOT NULL,
   parent_id         INTEGER                     DEFAULT NULL,
-  parents_ids       INTEGER[]                   DEFAULT NULL,
   user_id           INTEGER       NOT NULL      DEFAULT NULL, -- IN MIGRATION CHANGE TO USER ID
   create_at         TIMESTAMP                   DEFAULT CURRENT_TIMESTAMP,
   update_at         TIMESTAMP                   DEFAULT CURRENT_TIMESTAMP
