8ee8eff816ed65a8c6cca571ccc08db4f09b9e1f
David Xavier <davidxavier7512@gmail.com>
2025-10-17 23:04:31 +0100

    Working in folders

---
+++ node/db/migrations/1760085632_create_folders_table.js
@@ -0,0 +1,50 @@
+import MigrationTransaction from "../migrator/types/transaction.js";
+
+export default class CreateFoldersTable extends MigrationTransaction {
+
+  static async up () {
+
+    // ... create_table_folders ...
+    await this.execute(`
+      DROP FUNCTION IF EXISTS sharded.create_table_folders;
+      CREATE OR REPLACE FUNCTION sharded.create_table_folders (
+        IN _user_id integer,
+        IN _schema  text
+      ) RETURNS void AS $BODY$
+      DECLARE
+        _query  text;
+      BEGIN
+
+        _query := format($$
+          CREATE TABLE %1$I.folders (
+            id                SERIAL        PRIMARY KEY,
+            description       TEXT          NOT NULL,
+            parent_id         INTEGER                     DEFAULT NULL                REFERENCES %1$I.folders,
+            user_id           INTEGER       NOT NULL      DEFAULT %2$s                REFERENCES public.users, -- IN MIGRATION CHANGE TO USER ID
+            user_defined       BOOLEAN       NOT NULL      DEFAULT TRUE,
+            create_at         TIMESTAMP                   DEFAULT CURRENT_TIMESTAMP,
+            update_at         TIMESTAMP                   DEFAULT CURRENT_TIMESTAMP
+          );
+        $$, _schema, _user_id);
+
+        EXECUTE _query;
+
+        _query := format($$
+          INSERT INTO %1$I.folders (id, description, user_defined) VALUES (0, 'system', FALSE);
+        $$, _schema);
+
+        EXECUTE _query;
+
+        RETURN;
+      END;
+      $BODY$ LANGUAGE 'plpgsql';
+    `);
+
+
+  }
+
+  static async down () {
+
+  }
+
+}
--- node/src/jobs/folders/folders-ops.js
+++ node/src/jobs/folders/folders-ops.js
@@ -11,7 +11,9 @@ export default class FoldersOps extends Job {
       case 'POST':
         await this.create_folder();
         break;
-      case 'PATCH': break;
+      case 'PATCH':
+        await this.update_folder();
+        break;
       default:
         return this.reportError({status: 405, message: "Method Not Allowed"});
     }
@@ -22,11 +24,22 @@ export default class FoldersOps extends Job {
 
   async create_folder () {
     const { description, parent_id } = this.job.body;
-    const folder = await this.db.query(`INSERT INTO ${this.job.user_schema}.folders (description, parent_id) VALUES ($1, $2)`, [description, parent_id]);
+    const folder = await this.db.query(`INSERT INTO ${this.job.user_schema}.folders (description, parent_id) VALUES ($1, $2) RETURNING id, description, parent_id`, [description, parent_id]);
     return this.sendResponse({message: 'Folder successfully created', response: folder.rows[0]});
   }
 
-  async update_folder () {}
+  async update_folder () {
+
+    const match = this.req.url.match(/^\/folders\/(\d+)/);
+    if (!match || !match[1]) {
+      return this.reportError({ message: "Need indicate the item wuant update in url"});
+    }
+    const id = match[1];
+    const { description, parent_id } = this.job.body;
+    const folder = await this.db.query(`UPDATE ${this.job.user_schema}.folders SET description = $1, parent_id = $2 WHERE id = $3 RETURNING id, description, parent_id`, [description, parent_id, id]);
+    return this.sendResponse({message: 'Folder successfully updated', response: folder.rows[0]});
+  }
+
   async delete_folder () {}
 
 }
